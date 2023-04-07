60de3108c8051122bfcb18d1aa74d1de32e1732c
priyaas21 <80244559+priyaas21@users.noreply.github.com>
2023-04-07 06:55:38 +0530

    Added product json and created other folders

---
+++ src/components/productDetails.js
@@ -0,0 +1,11 @@
+import '../assets/css/productDetails.css';
+
+function ProductDetails() {
+  return (
+    <div className="page-container">
+      <h1>Product Details</h1>
+    </div>
+  );
+}
+
+export default ProductDetails;
